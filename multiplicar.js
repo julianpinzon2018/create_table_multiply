cc9adb81e1876b2226c927ba19310ecfa4540747
Julian Pinzon <julianpinzon2018@gmail.com>
2023-06-06 11:37:35 -0500

    multiplicar.js y multiplicar2.js add gitignore

--- multiplicar.js
+++
@@ -1,9 +0,0 @@
-const { crearArchivo } = require("./multiplicar1");
-
-console.clear();
-
-// let base = 5;
-
-crearArchivo(base)
-  .then((tabla) => console.log(tabla))
-  .catch((err) => console.log(err));
